6ae943f5cd13fba43c27cc732e28f4b0cb277583
Piggy <gongzuoshizcr@gmail.com>
2025-12-11 07:58:01 +0800

    learn pydantic

--- docs/notes01.md
+++ docs/notes01.md
@@ -7,3 +7,14 @@
 # 启动fastapi, reload 自动 reload
 uvicorn main:app --reload
 ```
+
+## 3. 笔记
+a.b 访问 b， a 是类的实例
+a['b'] 访问 b， a 是 dict
+将 pydantic 数据 改为 dict, 注意，原 new_post 类型不会改变
+```python
+new_dict = new_post.model_dump()
+```
+
+## video timestamp
+1:22:51
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -9,3 +18,10 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "this is your posts"}
+
+@app.post("/createposts") 
+def create_posts(new_post: Post):
+    new_dict = new_post.model_dump()
+    print(new_post)
+    print(new_dict)
+    return {"data": new_dict}
